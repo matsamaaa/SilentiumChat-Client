5853a2dd539bd51de5ed81073166489685681754
mat_sama <mateosejourne@gmail.com>
2025-08-22 01:43:44 +0200

    feat(navigation store):add

---
+++ pages/error/[code].vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="text-center mt-20">
+        <h1 class="text-5xl font-bold">{{ code }}</h1>
+        <p class="mt-4 text-xl">{{ message }}</p>
+    </div>
+</template>
+
+<script setup>
+const route = useRoute()
+
+// Récupérer le code depuis l'URL
+const code = route.params.code
+
+// Récupérer le message depuis les query params ou fallback
+const message = route.query.message || 'Une erreur est survenue'
+</script>
---
+++ stores/navigation.js
@@ -0,0 +1,22 @@
+import { defineStore } from 'pinia'
+import { useApiStore } from './api';
+import { useRouter } from '#imports'
+
+export const useNavigationStore = defineStore('navigation', {
+    actions: {
+        goToError(error = 404, message = 'Page not found') {
+            const router = useRouter();
+            router.push({ name: 'error', params: { error, message } });
+        },
+
+        goToRegister() {
+            const router = useRouter();
+            router.push({ name: 'register' });
+        },
+
+        goToLogin() {
+            const router = useRouter();
+            router.push({ name: 'login' });
+        }
+    }
+});
