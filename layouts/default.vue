db2c4cec616e7bdb958b101f2c6960e9253107c2
Mateo Sejourne <mateo.sejourne@etudiant.univ-lr.fr>
2025-12-11 09:42:59 +0100

    fix(crash route without login):update

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,8 +5,8 @@
         </div>
         <template v-else>
             <div class="flex flex-row overflow-y-auto overflow-x-hidden h-[100vh] w-[100vw]" >
-                <ServersBar />
-                <ProfilePlate />
+                <ServersBar v-if="connectedRoute" />
+                <ProfilePlate v-if="connectedRoute" />
 
                 <main class="flex-1 bg-gray-900">
                     <slot />
@@ -23,7 +23,6 @@ import { useUserStore } from '@/stores/user'
 import { useNavigationStore } from '@/stores/navigation'
 import { useWebSocketStore } from '@/stores/ws'
 import { useRoute } from 'vue-router'
-import AvatarIcon from '~/components/users/AvatarIcon.vue'
 import Notification from '~/components/popups/Notifications.vue'
 import ServersBar from '~/components/pages/layouts/ServersBar.vue'
 import ProfilePlate from '~/components/pages/layouts/ProfilePlate.vue'
@@ -33,15 +32,19 @@ const navigationStore = useNavigationStore()
 const webSocketStore = useWebSocketStore()
 const route = useRoute()
 
+const connectedRoute = ref(false);
+
 const checkConnectionAndRoute = (routeName) => {
     if (!userStore.isLoggedIn) {
         const isRouteWithoutConnection = navigationStore.isRouteWithoutConnection(routeName);
         if (!isRouteWithoutConnection) {
             navigationStore.goToLogin();
         }
+        connectedRoute.value = false;
         //if (route.name !== 'login' && route.name !== 'register') navigationStore.goToLogin();
     } else {
         webSocketStore.connect(userStore.user.uniqueId, userStore.token);
+        connectedRoute.value = true;
     }
 }
 
