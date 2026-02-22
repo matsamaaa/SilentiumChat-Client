c7f180048a909c07765b9a7e27bb8fcf1cf03001
unknown <mateosejourne@gmail.com>
2026-02-22 04:00:12 +0100

    feat(server selection):add

--- components/features/ServersWidget.vue
+++ components/features/ServersWidget.vue
@@ -8,22 +8,29 @@
         ]"
         class="bg-gray-900 flex flex-col items-center py-4 border-r border-gray-800">
 
-        <div class="flex flex-col gap-2">
+        <div class="flex flex-col gap-2 w-full items-center">
             <SquareIconButton icon="fa-solid fa-message" @click="navigationStore.goToHome" />
             
             <SquareIconButton icon="fa-solid fa-server" @click="navigationStore.goToServerCreation" />
             
-            <hr class="border-gray-700">
+            <hr class="border-gray-700 w-2/4" />
 
-            <SquareIconButton
+            <div
                 v-for="(server, code) in serversStore.servers"
                 :key="code"
-                :img-src="server.icon"
-                img-alt="Server Icon"
-                icon="fa-solid fa-server"
-                @click="navigationStore.goToServer(code)"
+                class="relative w-full flex justify-center"
             >
-            </SquareIconButton>
+                <span
+                    v-if="isActiveServer(code)"
+                    class="absolute left-0 top-1/2 -translate-y-1/2 w-2 h-8 bg-indigo-600 rounded-r-full"
+                />
+                <SquareIconButton
+                    :img-src="server.icon"
+                    img-alt="Server Icon"
+                    icon="fa-solid fa-server"
+                    @click="navigationStore.goToServer(code)"
+                />
+            </div>
         </div>
     </div>
 </template>
@@ -34,7 +41,15 @@ import { useNavigationStore, useServersStore } from '#imports';
 
 import { useDeviceStore } from '#imports';
 
+const route = useRoute();
+
 const navigationStore = useNavigationStore();
 const serversStore = useServersStore();
 const deviceStore = useDeviceStore();
+
+const isActiveServer = (code) => {
+    const current = route.params?.code;
+    if (!current || current !== code) return false;
+    return !!serversStore.getServerByCode(current);
+};
 </script>
